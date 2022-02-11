522c157763fb41d02c9ff3d8892f3e46b59e0029
vicente <vicente.real02@gmail.com>
2022-02-11 09:56:22 +0100

    Finalizando weatherApp

--- public/main.js
+++ public/main.js
@@ -1,3 +1,27 @@
-$(function(){
-    alert('Funcionando!!')
+
+navigator.geolocation.getCurrentPosition(async position => {
+    const parrafo = document.querySelector('#parrafo')
+    const {latitude, longitude} = position.coords
+
+    try{
+        const res = await fetch(`/lat/${latitude}/lon/${longitude}`)
+        const data = await res.json()
+
+        parrafo.innerHTML = `Pero donde estás hace ${parseInt(data.main.temp)} Cº`
+        
+        setInterval(async () => {
+            const res = await fetch(`/lat/${latitude}/lon/${longitude}`)
+            const data = await res.json()
+            parrafo.innerHTML = `Pero donde estás hace ${parseInt(data.main.temp)} Cº`
+        },60000)
+        
+
+    }catch(err){
+        console.log(err)
+    }
+
 })
+    
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,24 @@
+const { default: axios } = require('axios');
 var express = require('express');
 const { json } = require('express/lib/response');
 var router = express.Router();
 var request = require('request')
 var api = 'bf6ea4403d8d11b4438fbf6be848d881'
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/latitud/:latitud/longitud/:longitud', (req, res, next) => {
-  // console.log(req.params.latitud, req.params.longitud)
+router.get('/lat/:lat/lon/:lon',async (req, res, next) => {
 
-  if (!req.params.latitud || !req.params.longitud){
-    res.status(404)
-  }
+  if (!req.params.latitud || !req.params.longitud) res.status(404)
+  
+  var latitud = parseInt(req.params.lat, 10) //39.4702
+  var longitud = parseInt(req.params.lon, 10) //-0.37680375
 
-  var latitud = parseInt(req.params.latitud, 10)
-  var longitud = parseInt(req.params.longitud, 10)
-
-  request('https://api.openweathermap.org/data/2.5/weather?lat=39.4702&lon=-0.376805&appid=bf6ea4403d8d11b4438fbf6be848d881',{json: true},
-  (err, res, body) => {
-    if(err) {return console.log(err)}
-    console.log(body)
-  })
-
-  res.end('Recibido')
+  const peticion = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${api}&units=metric`)
+  const data = peticion.data
+  res.json(data)
 })
 
 module.exports = router;
